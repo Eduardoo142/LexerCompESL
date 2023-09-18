574c65e246b07899eefd5f4792dff1d039dc1230
Eduardoo142 <earangos142@gmail.com>
2023-09-17 22:44:02 -0500

    Operaciones

--- ast.dart
+++ ast.dart
@@ -2,7 +2,6 @@ import 'tokens.dart';
 
 abstract class ASTNode {
   String tokenLiteral();
-  String toString();
 }
 
 class Statement extends ASTNode {
@@ -14,33 +13,21 @@ class Statement extends ASTNode {
   String tokenLiteral() {
     return token.literal;
   }
-
-  @override
-  String toString() {
-    return token.literal;
-  }
 }
 
-class Expression extends ASTNode {
-  final Token token;
+abstract class Expression implements ASTNode {
+  Token token;
 
   Expression(this.token);
 
   @override
-  String tokenLiteral() {
-    return token.literal;
-  }
-
-  @override
-  String toString() {
-    return token.literal;
-  }
+  String tokenLiteral() => token.literal;
 }
 
 class Program extends ASTNode {
-  final List<Statement> statements;
+  late List<Statement> statements;
 
-  Program(this.statements);
+  Program({required this.statements});
 
   @override
   String tokenLiteral() {
@@ -52,6 +39,132 @@ class Program extends ASTNode {
 
   @override
   String toString() {
-    return statements.map((statement) => statement.toString()).join('');
+    List<String> out = [];
+    for (var statement in statements) {
+      out.add(statement.toString());
+    }
+    return out.join('');
+  }
+}
+
+class Identifier extends Expression {
+  String value;
+
+  Identifier(Token token, this.value) : super(token);
+
+  @override
+  String toString() => value;
+}
+
+class LetStatement extends Statement {
+  late final Identifier? name;
+  late final Expression? value;
+
+  LetStatement(Token token, {this.name, this.value}) : super(token);
+
+  @override
+  String toString() => '${tokenLiteral()} $name = $value;';
+}
+
+class ReturnStatement extends Statement {
+  late final Expression? returnValue;
+
+  ReturnStatement(Token token, {this.returnValue}) : super(token);
+
+  @override
+  String toString() => '${tokenLiteral()} ${returnValue.toString()};';
+}
+
+class ExpressionStatement extends Statement {
+  late final Expression? expression;
+
+  ExpressionStatement(Token token, Expression expression) : 
+  expression = expression,
+  super(token);
+
+  @override
+  String toString() => expression.toString();
+}
+
+class Integer extends Expression {
+  int? value;
+
+  Integer(Token? token, {int? value}) : super(token!){
+    this.value = value;
+  }
+
+  @override
+  String toString() => value.toString();
+}
+
+class Prefix extends Expression {
+  final String operator;
+  late final Expression? right;
+
+  Prefix(Token token, this.operator, {this.right}) : super(token);
+
+  @override
+  String toString() => '($operator${right.toString()})';
+}
+
+class Infix extends Expression {
+  final Expression left;
+  final String operator;
+  late final Expression? right;
+
+  Infix(Token token, this.left, this.operator, {this.right}) : super(token);
+
+  @override
+  String toString() => '(${left.toString()} $operator ${right.toString()})';
+}
+
+class Boolean extends Expression {
+  final bool? value;
+
+  Boolean(Token token, bool bool, {this.value}) : super(token);
+
+  @override
+  String toString() => tokenLiteral();
+}
+
+class Block extends Statement {
+  List<Statement> statements;
+
+  Block(Token token, this.statements) : super(token);
+
+  @override
+  String toString() {
+    final out = <String>[];
+    for(final statement in statements){
+      out.add(statement.toString());
+    }
+    return out.join('');
+  }
+}
+
+class Funcion extends Expression {
+  late final List<Identifier> parameters;
+  late final Block? body;
+
+  Funcion(Token token, {this.parameters = const [], this.body}) : super(token);
+
+  @override
+  String toString() {
+    String paramList = parameters.map((param) => param.toString()).join(', ');
+    return '${tokenLiteral()}($paramList) ${body.toString()}';
+  }
+}
+
+class Call extends Expression {
+  Expression function;
+  List<Expression>? arguments;
+
+  Call(Token token, this.function, {this.arguments}) : super(token);
+
+  @override
+  String toString() {
+    assert(arguments != null);
+    final argList = arguments!.map((arg) => arg.toString()).join(', ');
+    return '${function.toString()}($argList)';
   }
 }
--- tokens.dart
+++ tokens.dart
@@ -1,38 +1,91 @@
-class TokenType {
-  static const ASSIGN = "ASIGNACION";
-  static const PLUS = "SUMA";
-  static const EOF = "EOF";
-  static const LPAREN = "PARENIZQ";
-  static const RPAREN = "PARENDER";
-  static const LBRACE = "LLAVEIZQ";
-  static const RBRACE = "LLAVEDER";
-  static const COMMA = "COMA";
-  static const SEMICOLON = "PUNTOYCOMA";
-  static const MINUS = "MENOS";
-  static const DIVISION = "DIVISION";
-  static const MULTIPLICATION = "MULTIPLICACION";
-  static const LT = "LT";
-  static const GT = "GT";
-  static const NOT_EQ = "NO_EQ";
-  static const NEGATION = "NEGACION";
-  static const INT = "INT";
-  static const ILLEGAL = "ILEGAL";
-  static const FALSE = "FALSO";
-  static const FUNCTION = "FUNCION";
-  static const LET = "LET";
-  static const TRUE = "VERDADERO";
-  static const IF = "SI";
-  static const ELSE = "SINO";
-  static const RETURN = "RETORNAR";
-  static const EQ = "EQ";
-  static const STRING = "STRING";
-  static const COLON = "COLON";
-  static const IDENTIFIER = "IDENTIFICADOR";
+//class TokenType {
+//  static const ASSIGN = "ASIGNACION";
+//  static const PLUS = "SUMA";
+//  static const EOF = "EOF";
+//  static const LPAREN = "PARENIZQ";
+//  static const RPAREN = "PARENDER";
+//  static const LBRACE = "LLAVEIZQ";
+//  static const RBRACE = "LLAVEDER";
+//  static const COMMA = "COMA";
+//  static const SEMICOLON = "PUNTOYCOMA";
+//  static const MINUS = "MENOS";
+//  static const DIVISION = "DIVISION";
+//  static const MULTIPLICATION = "MULTIPLICACION";
+//  static const LT = "LT";
+//  static const GT = "GT";
+//  static const NOT_EQ = "NO_EQ";
+//  static const NEGATION = "NEGACION";
+//  static const INT = "INT";
+//  static const ILLEGAL = "ILEGAL";
+//  static const FALSE = "FALSO";
+//  static const FUNCTION = "FUNCION";
+//  static const LET = "LET";
+//  static const TRUE = "VERDADERO";
+//  static const IF = "SI";
+//  static const ELSE = "SINO";
+//  static const RETURN = "RETORNAR";
+//  static const EQ = "EQ";
+//  static const STRING = "STRING";
+//  static const COLON = "COLON";
+//  static const IDENTIFIER = "IDENTIFICADOR";
+//}
+
+enum TokenType {
+  ASSIGN,
+  PLUS,
+  EOF,
+  LPAREN,
+  RPAREN,
+  LBRACE,
+  RBRACE,
+  COMMA,
+  SEMICOLON,
+  MINUS,
+  DIVISION,
+  MULTIPLICATION,
+  LT,
+  GT,
+  NOT_EQ,
+  NEGATION,
+  INT,
+  ILLEGAL,
+  FALSE,
+  FUNCTION,
+  LET,
+  TRUE,
+  IF,
+  ELSE,
+  RETURN,
+  EQ,
+  STRING,
+  COLON,
+  IDENTIFIER,
+  DIF,
+  INTEGER,
 }
 
 class Token {
-  final String type;
+  final TokenType token_type;
   final String literal;
 
-  Token(this.type, this.literal);
+  Token(this.token_type, this.literal);
+  @override
+  String toString() {
+    return 'Type: $token_type, Literal $literal';
+  }
 }
+
+TokenType lookupTokenType(String literal) {
+  final keywords = {
+    'funcion': TokenType.FUNCTION,
+    'variable': TokenType.LET,
+    'si': TokenType.IF,
+    'si_no': TokenType.ELSE,
+    'regresa': TokenType.RETURN,
+    'verdadero': TokenType.TRUE,
+    'falso': TokenType.FALSE,
+  };
+
+  return keywords[literal] ?? TokenType.IDENTIFIER;
+}
+
